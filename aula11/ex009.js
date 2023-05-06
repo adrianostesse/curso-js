eca6fcec2a5ae794dcb0149969743847819522e3
Adriano Stesse <123905524+adrianostesse@users.noreply.github.com>
2023-05-05 23:09:22 -0300

    criei uns exercicios e tenho q ver o erro do 10-2

---
+++ aula11/ex009.js
@@ -0,0 +1,15 @@
+var país = 'EUA'
+    console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log ('Brasileiro')
+} else {
+    console.log ('Gringo')
+}
+
+var p = 'brasil'
+console.log(`vivendo em ${p}`)
+if (p == 'brasil') {
+    console.log ('brasileiro')
+} else {
+    console.log('gringo')
+}
