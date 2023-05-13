d2fb664ee43b5d7b601139af29a639c78227ee5b
Adriano Stesse <123905524+adrianostesse@users.noreply.github.com>
2023-05-12 22:20:49 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+//ESSE EM BAIXO É REPETIÇÃO COM DO...WHILE / TEM O TESTE LOGICO(CONDIÇÃO) NO FINAL
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++ // c++ signifca c = c+1 (c é a variavel, só pra n esquecer)
+}while(c <= 6)
+
+/*
+REPETIÇÃO COM WHILE / TEM O TESTE LOGICO(CONDIÇÃO) NO INICIO
+var c = 1
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c++ signifca c = c+1 (c é a variavel, só pra n esquecer)
+}
+*/
+
+/*
+console.log('papapa')
+console.log('papapa')
+console.log('papapa')
+console.log('papapa')
+console.log('papapa')
+console.log('papapa')
+*/
